3ade128bb243f5f99809daa5f7b20ad01ae4d38e
B2 <jonathanr.baker2007@gmail.com>
2024-10-09 13:20:35 -0500

    updated flask

--- API/Web/webRun.py
+++ API/Web/webRun.py
@@ -1,13 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'  # Needed for flash messages
 
-# Route for the homepage
-@app.route('/')
-def home():
-    return render_template('index.html')  # Assumes you have 'index.html' in a 'templates' folder
+# Dummy user for login authentication
+users = {
+    "chef": "Sictc1!",
+    "admin": "BakerHammonds"
+}
+
+# Route for the login page
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username and password match the dummy user
+        if username in users and users[username] == password:
+            return redirect(url_for('data'))  # Redirect to data page
+        else:
+            flash('Invalid login. Please try again.')
+            return render_template('index.html')
+
+    return render_template('index.html')
+
+# Route for the data page (after login)
+@app.route('/data')
+def data():
+    if request.method == 'POST':
+        return render_template('index.html')
+        
+    return render_template('data.html')
 
 if __name__ == '__main__':
-    # Run the server on the local network
     app.run(host='0.0.0.0', port=4000, debug=True)
-
